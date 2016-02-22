3e24d97e08daee60f370995d07398da8f7e6f08f
champsupertramp <heychampsupertramp@gmail.com>
2016-02-22 15:32:37 +0800

    Remove notices

--- admin/core/um-admin-roles.php
+++ admin/core/um-admin-roles.php
@@ -42,7 +42,7 @@ class UM_Admin_Roles {
 					$role = $post->post_name;
 				}
 				$data = $ultimatemember->query->role_data($role);
-				if ( $data['can_access_wpadmin'] ){
+				if ( isset( $data['can_access_wpadmin'] ) ){
 					echo '<span class="um-adm-ico um-admin-tipsy-n" title="'.__('This role can access the WordPress backend','ultimatemember').'"><i class="um-faicon-check"></i></span>';
 				} else {
 					echo __('No','ultimatemember');
