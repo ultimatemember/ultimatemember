f785a4d065203ddbf6ec76d59b634315694600a7
jonfalcon <darkutubuki143@gmail.com>
2016-04-05 16:10:54 -0700

    Fix random user function

--- core/um-filters-members.php
+++ core/um-filters-members.php
@@ -238,13 +238,14 @@
 	/***
 	***	@sorting random
 	***/
-	add_filter('pre_user_query', function($query) {
+	add_filter( 'pre_user_query', 'um_sortby_random' );
+	function um_sortby_random( $query ) {
 	   if($query->query_vars["orderby"] == 'random') {
 	       $query->query_orderby = 'ORDER by RAND()';
 	   }
 
 	   return $query;
-	});
+	}
 
 	/***
 	***	@hook in the member results array
