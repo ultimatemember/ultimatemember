79f81e4e4b65afd62e861eb2b640cadb25865e94
champsupertramp <heychampsupertramp@gmail.com>
2016-09-19 18:36:46 +0800

    Fix Password Reset Email option's description

--- um-config.php
+++ um-config.php
@@ -842,7 +842,7 @@ $this->sections[] = array(
                 'type'     => 'switch',
                 'title'    => __( 'Password Reset Email','ultimatemember' ),
 				'default'  => 1,
-				'desc' 	   => __('Whether to send the user an email when he request to reset password (Recommended, please keep on)','ultimatemember'),
+				'desc' 	   => __('Whether to send an email when users changed their password (Recommended, please keep on)','ultimatemember'),
         ),
 
         array(
