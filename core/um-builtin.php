74c17a77ce992ffc45d7fa956f4510fdd0fdcee1
champsupertramp <heychampsupertramp@gmail.com>
2016-09-20 09:38:50 +0800

    Allow textarea in profile tagline

--- core/um-builtin.php
+++ core/um-builtin.php
@@ -1082,7 +1082,7 @@ class UM_Builtin {
 		$fields_without_metakey = apply_filters('um_fields_without_metakey', $fields_without_metakey );
 		
 		if ( !$show_all ) {
-			$this->fields_dropdown = array('image','file','password','textarea','rating');
+			$this->fields_dropdown = array('image','file','password','rating');
 			$this->fields_dropdown = array_merge( $this->fields_dropdown, $fields_without_metakey );
 		} else {
 			$this->fields_dropdown = $fields_without_metakey;
