a5627717173a93ad102a08d9de7cbf6bb2cd003a
champsupertramp <heychampsupertramp@gmail.com>
2016-09-22 19:32:43 +0800

    Fix typo in welcome email template

--- um-config.php
+++ um-config.php
@@ -738,7 +738,7 @@ $this->sections[] = array(
 										  '{login_url}'  . "\r\n\r\n" .
 										  'Your account e-mail: {email}' . "\r\n" .
 										  'Your account username: {username}' . "\r\n" .
-										  'Set your account passowrd: {password_reset_link}' . "\r\n\r\n" .
+										  'Set your account password: {password_reset_link}' . "\r\n\r\n" .
 										  'If you have any problems, please contact us at {admin_email}'  . "\r\n\r\n" .
 										  'Thanks,' . "\r\n" .
 										  '{site_name}',
