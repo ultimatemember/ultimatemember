9b1419bfd60a319c83643a7c77f9f5fe052017d0
Champ Camba <heychampsupertramp@gmail.com>
2017-01-02 22:00:13 +0800

    Update system info

--- addons/system_info.php
+++ addons/system_info.php
@@ -317,6 +317,7 @@ FSOCKOPEN:                			<?php echo ( function_exists( 'fsockopen' ) ) ? 'Yo
 cURL:                     				<?php echo ( function_exists( 'curl_init' ) ) ? 'Your server supports cURL.' : 'Your server does not support cURL.'; ?><?php echo "\n"; ?>
 SOAP Client:              			<?php echo ( class_exists( 'SoapClient' ) ) ? 'Your server has the SOAP Client enabled.' : 'Your server does not have the SOAP Client enabled.'; ?><?php echo "\n"; ?>
 SUHOSIN:                  			<?php echo ( extension_loaded( 'suhosin' ) ) ? 'Your server has SUHOSIN installed.' : 'Your server does not have SUHOSIN installed.'; ?><?php echo "\n"; ?>
+GD Library:                  			<?php echo ( extension_loaded( 'gd' ) && function_exists('gd_info') ) ? 'PHP GD library is installed on your web server.' : 'PHP GD library is NOT installed on your web server.'; ?><?php echo "\n"; ?>
 
 
 --- Session Configurations ---
