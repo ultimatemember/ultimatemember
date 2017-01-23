754368505ef56b2b514109d944c5ddc5c2453178
Champ Camba <heychampsupertramp@gmail.com>
2017-01-23 20:26:38 +0800

    Fix author name in recent comments widget

--- core/um-filters-commenting.php
+++ core/um-filters-commenting.php
@@ -3,24 +3,30 @@
 	/***
 	***	@Control comment author display
 	***/
-	add_filter('get_comment_author_link', 'um_comment_link_to_profile', 10000 );
-	function um_comment_link_to_profile( $return ) {
-		global $comment, $ultimatemember;
-		if ( isset( $comment->user_id ) && !empty( $comment->user_id ) ) {
+	add_filter('get_comment_author_link', 'um_comment_link_to_profile', 10000, 3 );
+	function um_comment_link_to_profile( $return, $author, $comment_ID ) {
+		global $ultimatemember;
+
+			$comment = get_comment( $comment_ID );
 			
-			if ( isset( $ultimatemember->user->cached_user[ $comment->user_id ] ) && $ultimatemember->user->cached_user[ $comment->user_id ] ) {
+			if( isset( $comment->user_id ) && ! empty(  $comment->user_id ) ){	
+				if ( isset( $ultimatemember->user->cached_user[ $comment->user_id ] ) && $ultimatemember->user->cached_user[ $comment->user_id ] ) {
+					
+					$return = '<a href="'. $ultimatemember->user->cached_user[$comment->user_id]['url'] . '">' . $ultimatemember->user->cached_user[$comment->user_id]['name'] . '</a>';
 				
-				$return = '<a href="'. $ultimatemember->user->cached_user[$comment->user_id]['url'] . '">' . $ultimatemember->user->cached_user[$comment->user_id]['name'] . '</a>';
-			
-			} else {
+				} else {
+					
+					um_fetch_user( $comment->user_id );
 				
-				um_fetch_user($comment->user_id);
-				$ultimatemember->user->cached_user[ $comment->user_id ] = array('url' => um_user_profile_url(), 'name' => um_user('display_name') );
-				$return = '<a href="'. $ultimatemember->user->cached_user[$comment->user_id]['url'] . '">' . $ultimatemember->user->cached_user[$comment->user_id]['name'] . '</a>';
-				um_reset_user();
+					$ultimatemember->user->cached_user[ $comment->user_id ] = array('url' => um_user_profile_url(), 'name' => um_user('display_name') );
+					$return = '<a href="'. $ultimatemember->user->cached_user[$comment->user_id]['url'] . '">' . $ultimatemember->user->cached_user[$comment->user_id]['name'] . '</a>';
+
+					um_reset_user();
 				
+				}
 			}
-			
-		}
+
 		return $return;
 	}
+
+	
