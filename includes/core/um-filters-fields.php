1efbea2e2fdc8abfea35fabd83530c4b9f0dd56e
Champ Camba <heychampsupertramp@gmail.com>
2017-10-04 10:35:05 +0800

    Fix invalid call user function error

--- includes/core/um-filters-fields.php
+++ includes/core/um-filters-fields.php
@@ -466,7 +466,7 @@
 	 * @uses   hook filters: um_profile_field_filter_hook__
 	 */
     add_filter('um_profile_field_filter_hook__','um_profile_field_filter_xss_validation',10,2);
-    function um_profile_field_filter__xss_validation( $value, $data ){
+    function um_profile_field_filter_xss_validation( $value, $data ){
 	    if( ! empty( $value ) && is_string($value)){
 		    $value = stripslashes( $value );
 		    $data['validate'] = isset( $data['validate'] ) ? $data['validate'] : '';
