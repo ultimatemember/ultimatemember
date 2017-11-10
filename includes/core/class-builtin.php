1c1fb2c36996475bfd3d4c2c1674c0c151bf2932
nikitozzzzzzz <nsinelnikov.work@gmail.com>
2017-11-10 12:10:59 +0200

    - fixed role predefined fields values;

--- includes/core/class-builtin.php
+++ includes/core/class-builtin.php
@@ -543,9 +543,15 @@ if ( ! class_exists( 'Builtin' ) ) {
         function set_predefined_fields() {
 
 	        global $wp_roles;
-	        $role_keys = array_map( function( $item ) {
-		        return 'um_' . $item;
-	        }, get_option( 'um_roles' ) );
+	        $role_keys = get_option( 'um_roles' );
+	        if ( ! empty( $role_keys ) && is_array( $role_keys ) ) {
+		        $role_keys = array_map( function( $item ) {
+			        return 'um_' . $item;
+		        }, $role_keys );
+	        } else {
+		        $role_keys = array();
+	        }
+
 			$exclude_roles = array_diff( array_keys( $wp_roles->roles ), array_merge( $role_keys, array( 'subscriber' ) ) );
 
             $um_roles = UM()->roles()->get_roles( false, $exclude_roles );
