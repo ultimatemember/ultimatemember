db5f646217af9028dad612d5bf4c9e1d5df84132
nikitozzzzzzz <nsinelnikov.work@gmail.com>
2018-03-07 11:35:28 +0200

    - small fixes; - added some translations;

--- includes/admin/templates/dashboard/cache.php
+++ includes/admin/templates/dashboard/cache.php
@@ -1,17 +1,15 @@
-<?php 
-
-$all_options = wp_load_alloptions();
+<?php $all_options = wp_load_alloptions();
 
 $count = 0;
 foreach ( $all_options as $k => $v ) {
-	
 	if ( strstr( $k, 'um_cache_userdata_' ) !== false ) {
 		$count++;
 	}
-	
-}
+} ?>
 
- ?>
-
-<p>Run this task from time to time to keep your DB clean.</p>
-<p><a href="<?php echo add_query_arg( 'um_adm_action', 'user_cache' ); ?>" class="button">Clear cache of <?php echo $count; ?> users</a></p>
+<p><?php _e( 'Run this task from time to time to keep your DB clean.', 'ultimate-member' ) ?></p>
+<p>
+	<a href="<?php echo add_query_arg( 'um_adm_action', 'user_cache' ); ?>" class="button">
+		<?php printf( __( 'Clear cache of %s users', 'ultimate-member' ), $count ) ?>
+	</a>
+</p>
