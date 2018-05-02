b23f76291980b19ca90d4d5772f84520aaee44a4
nikitozzzzzzz <nsinelnikov.work@gmail.com>
2018-05-02 15:46:09 +0300

    - fixed Profile menu on some installs; - typo fixed;

--- includes/admin/templates/form/register_customize.php
+++ includes/admin/templates/form/register_customize.php
@@ -80,7 +80,7 @@
 			array(
 				'id'		    => '_um_register_secondary_btn_word',
 				'type'		    => 'text',
-				'label'    		=> __( 'Primary Button Text', 'ultimate-member' ),
+				'label'    		=> __( 'Secondary Button Text', 'ultimate-member' ),
 				'tooltip'    	=> __( 'Customize the button text', 'ultimate-member' ),
 				'value' 		=> UM()->query()->get_meta_value( '_um_register_secondary_btn_word', null, UM()->options()->get( 'register_secondary_btn_word' ) ),
 				'conditional'	=> array( '_um_register_secondary_btn', '=', 1 )
