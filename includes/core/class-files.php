3fe1f4907bb7b93e1bfa167818666496366f58eb
Champ Camba <heychampsupertramp@gmail.com>
2018-07-24 02:43:08 +0800

    Initial changes in image uploader and resize processor

--- includes/core/class-files.php
+++ includes/core/class-files.php
@@ -105,22 +105,13 @@ if ( ! class_exists( 'um\core\Files' ) ) {
 				wp_send_json_error( esc_js( __( 'Invalid coordinates', 'ultimate-member' ) ) );
 			}
 
-			$um_is_temp_image = um_is_temp_image( $src );
-			if ( ! $um_is_temp_image ) {
-				wp_send_json_error( esc_js( __( 'Invalid Image file', 'ultimate-member' ) ) );
+			$own_image_file = um_is_file_owner( $src, $user_id, true );
+			if ( ! $own_image_file ) {
+				wp_send_json_error( esc_js( __( 'Invalid file ownership', 'ultimate-member' ) ) );
 			}
+			
 
-			$crop = explode( ',', $coord );
-			$crop = array_map( 'intval', $crop );
-
-			$uri = UM()->files()->resize_image( $um_is_temp_image, $crop );
-
-			// If you're updating a user
-			if ( isset( $user_id ) && $user_id > 0 ) {
-				$uri = UM()->files()->new_user_upload( $user_id, $um_is_temp_image, $key );
-			}
-
-			$output = $uri;
+			$output = UM()->uploader()->resize_image( $own_image_file, $src, $key, $user_id, $coord );
 
 			delete_option( "um_cache_userdata_{$user_id}" );
 
@@ -569,13 +560,11 @@ if ( ! class_exists( 'um\core\Files' ) ) {
 		 *
 		 * @return mixed
 		 */
-		function get_image_data( $file ) {
-			
+		function get_image_data( $file ) {	
 
 			$finfo = finfo_open( FILEINFO_MIME_TYPE );
 			
-			$mime_type = finfo_file( $finfo, $file );
-				
+			$mime_type = finfo_file( $finfo, $file );		
 
 			if( function_exists('exif_imagetype') ){
 				
@@ -1209,6 +1198,7 @@ if ( ! class_exists( 'um\core\Files' ) ) {
 			UM()->fields()->set_id = $_POST['set_id'];
 			UM()->fields()->set_mode = $_POST['set_mode'];
 
+
 			/**
 			 * UM hook
 			 *
@@ -1233,32 +1223,25 @@ if ( ! class_exists( 'um\core\Files' ) ) {
 			$um_image_upload_nonce = apply_filters("um_image_upload_nonce", true );
 
 			if(  $um_image_upload_nonce ){
-				if ( ! wp_verify_nonce( $nonce, 'um_upload_nonce-'.$timestamp ) && is_user_logged_in() ) {
+				if ( ! wp_verify_nonce( $nonce, "um_upload_nonce-{$timestamp}" ) && is_user_logged_in() ) {
 					// This nonce is not valid.
 					$ret['error'] = 'Invalid nonce';
 					die( json_encode( $ret ) );
 				}
 			}
 
-			if(isset($_FILES[$id]['name'])) {
+			if( isset( $_FILES[ $id ]['name'] ) ) {
 
-				if(!is_array($_FILES[$id]['name'])) {
+				if( ! is_array( $_FILES[ $id ]['name'] ) ) {
 
-					$temp = $_FILES[$id]["tmp_name"];
-					$file = $id."-".$_FILES[$id]["name"];
-					$file = sanitize_file_name($file);
-					$ext = strtolower( pathinfo($file, PATHINFO_EXTENSION) );
+					$uploaded = UM()->uploader()->upload_image( $_FILES[ $id ], get_current_user_id(), $id );
+					if ( isset( $uploaded['error'] ) ){
 
-					$error = UM()->files()->check_image_upload( $temp, $id );
-					if ( $error ){
+						$ret['error'] = $uploaded['error'];
 
-						$ret['error'] = $error;
-
-					} else {
-						
-						$file = "stream_photo_".md5($file)."_".uniqid().".".$ext;
-						$ret[ ] = UM()->files()->new_image_upload_temp( $temp, $file, UM()->options()->get('image_compression') );
-				
+					}else{
+						$ts = current_time( 'timestamp' );
+						$ret[ ] = $uploaded['handle_upload']['url']."?{$ts}";
 					}
 
 				}
@@ -1266,6 +1249,8 @@ if ( ! class_exists( 'um\core\Files' ) ) {
 			} else {
 				$ret['error'] = __('A theme or plugin compatibility issue','ultimate-member');
 			}
+
+			
 			echo json_encode($ret);
 			exit;
 		}
