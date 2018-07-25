c44c23d6b16fc4f1c9868ef8bb7791fd83c92c69
Champ Camba <heychampsupertramp@gmail.com>
2018-07-26 00:36:05 +0800

    Fix ajax with nopriv

--- includes/core/class-ajax-common.php
+++ includes/core/class-ajax-common.php
@@ -33,15 +33,21 @@ if ( ! class_exists( 'um\core\AJAX_Common' ) ) {
 			}
 
 			add_action( 'wp_ajax_um_remove_file', array( UM()->files(), 'ajax_remove_file' ) );
-			add_action( 'wp_ajax_um_resize_image', array( UM()->files(), 'ajax_resize_image' ) );
 			add_action( 'wp_ajax_um_delete_profile_photo', array( UM()->profile(), 'ajax_delete_profile_photo' ) );
 			add_action( 'wp_ajax_um_delete_cover_photo', array( UM()->profile(), 'ajax_delete_cover_photo' ) );
 			add_action( 'wp_ajax_um_select_options', array( UM()->form(), 'ajax_select_options' ) );
 			add_action( 'wp_ajax_um_ajax_paginate', array( UM()->query(), 'ajax_paginate' ) );
 			add_action( 'wp_ajax_um_muted_action', array( UM()->form(), 'ajax_muted_action' ) );
+			
+			add_action( 'wp_ajax_nopriv_um_fileupload', array( UM()->files(), 'ajax_file_upload' ) );
 			add_action( 'wp_ajax_um_fileupload', array( UM()->files(), 'ajax_file_upload' ) );
+			
+			add_action( 'wp_ajax_nopriv_um_imageupload', array( UM()->files(), 'ajax_image_upload' ) );
 			add_action( 'wp_ajax_um_imageupload', array( UM()->files(), 'ajax_image_upload' ) );
-
+			
+			add_action( 'wp_ajax_nopriv_um_resize_image', array( UM()->files(), 'ajax_resize_image' ) );
+			add_action( 'wp_ajax_um_resize_image', array( UM()->files(), 'ajax_resize_image' ) );
+			
 
 			/**
 			 * Fallback for ajax urls
