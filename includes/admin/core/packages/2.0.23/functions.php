e4ac4cda8f13a9ff4883623a746389c444f343f7
nikitozzzzzzz <nsinelnikov.work@gmail.com>
2018-08-10 11:34:09 +0300

    - privacy account fix;

---
+++ includes/admin/core/packages/2.0.23/functions.php
@@ -0,0 +1,29 @@
+<?php
+function um_upgrade_privacy2023() {
+	um_maybe_unset_time_limit();
+
+	global $wpdb;
+
+	$wpdb->query( $wpdb->prepare(
+		"UPDATE {$wpdb->usermeta} 
+		SET meta_value = CASE 
+			WHEN meta_value = %s THEN 'Everyone' 
+			WHEN meta_value = %s THEN 'Only me' 
+			WHEN meta_value = %s THEN 'followed' 
+			WHEN meta_value = %s THEN 'follower' 
+			WHEN meta_value = %s THEN 'friends' 
+		END CASE
+		WHERE meta_key = 'profile_privacy'",
+		__( 'Everyone', 'ultimate-member' ),
+		__( 'Only me', 'ultimate-member' ),
+		__( 'Only people I follow can view my profile', 'um-followers' ),
+		__( 'Followers', 'um-followers' ),
+		__( 'Friends only', 'um-friends' )
+	) );
+
+	UM()->user()->remove_cache_all_users();
+
+	update_option( 'um_last_version_upgrade', '2.0.23' );
+
+	wp_send_json_success( array( 'message' => __( 'Privacy Settings was upgraded successfully', 'ultimate-member' ) ) );
+}
---
+++ includes/admin/core/packages/2.0.23/hooks.php
@@ -0,0 +1,5 @@
+<?php
+
+return array(
+	'privacy2023' => 'privacy2023',
+);
