dd03b4f325629b2c58de3571cae1534a45ed88ee
nikitozzzzzzz <nsinelnikov.work@gmail.com>
2018-08-13 14:42:49 +0300

    - filter for template loading;

--- includes/core/class-shortcodes.php
+++ includes/core/class-shortcodes.php
@@ -265,6 +265,8 @@ if ( ! class_exists( 'um\core\Shortcodes' ) ) {
 				unset( $args['theme_file'] );
 				unset( $args['tpl'] );
 
+				$args = apply_filters( 'um_template_load_args', $args, $tpl );
+
 				extract( $args );
 			}
 
