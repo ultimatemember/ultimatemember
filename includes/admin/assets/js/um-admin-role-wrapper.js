ce0324707df689e27fe9aa3d03a0009496de69fb
nikitasinelnikov <nsinelnikov.work@gmail.com>
2019-08-21 16:05:57 +0300

    - hotfix: if roles localized as object in some installations; - changed readme;

--- includes/admin/assets/js/um-admin-role-wrapper.js
+++ includes/admin/assets/js/um-admin-role-wrapper.js
@@ -1,6 +1,11 @@
 jQuery( document ).ready( function() {
 
 	jQuery( '#role' ).change( function() {
+
+		if ( typeof um_roles == 'object' ) {
+			um_roles = Object.keys( um_roles ).map(function( key ) { return um_roles[ key ]; });
+		}
+
 		if ( jQuery.inArray( jQuery(this).val().substr(3), um_roles ) !== -1 ) {
 			jQuery( '#um_role_selector_wrapper' ).hide();
 			jQuery( '#um-role' ).val('');
@@ -10,6 +15,10 @@ jQuery( document ).ready( function() {
 	}).trigger('change');
 
 	jQuery( '#adduser-role' ).change( function() {
+		if ( typeof um_roles == 'object' ) {
+			um_roles = Object.keys( um_roles ).map(function( key ) { return um_roles[ key ]; });
+		}
+
 		if ( jQuery.inArray( jQuery(this).val().substr(3), um_roles ) !== -1 ) {
 			jQuery( '#um_role_existing_selector_wrapper' ).hide();
 			jQuery( '#um-role' ).val('');
