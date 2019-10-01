ff9f6afe3152cd42842a6459c4c15b2a86426d62
nikitasinelnikov <nsinelnikov.work@gmail.com>
2019-10-01 15:34:30 +0300

    - fixed admin_post and admin_post_nopriv requests when user hasn't ability to see wp-admin;

--- includes/core/um-actions-wpadmin.php
+++ includes/core/um-actions-wpadmin.php
@@ -7,8 +7,18 @@ if ( ! defined( 'ABSPATH' ) ) exit;
  * Checks if user can access the backend
  */
 function um_block_wpadmin_by_user_role() {
-	if ( is_admin() && ! defined( 'DOING_AJAX' ) && um_user( 'ID' ) && ! um_user( 'can_access_wpadmin' ) && ! is_super_admin( um_user( 'ID' ) ) ) {
-		um_redirect_home();
+	if ( is_admin() && ! defined( 'DOING_AJAX' ) ) {
+		$action = empty( $_REQUEST['action'] ) ? '' : $_REQUEST['action'];
+
+		// filter that it's not admin_post or admin_post_nopriv request
+		$url_attr = parse_url( UM()->permalinks()->get_current_url() );
+		if ( is_user_logged_in() && ! empty( $action ) && $url_attr['path'] == '/wp-admin/admin-post.php' ) {
+			return;
+		}
+
+		if ( um_user( 'ID' ) && ! um_user( 'can_access_wpadmin' ) && ! is_super_admin( um_user( 'ID' ) ) ) {
+			um_redirect_home();
+		}
 	}
 }
 add_action( 'init', 'um_block_wpadmin_by_user_role', 99 );
