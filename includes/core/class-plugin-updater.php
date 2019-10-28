6f0ba1bccff36eb0954901675e5ea1e521500d80
andrewshuba <andrew.shuba2@gmail.com>
2019-10-28 13:28:14 +0200

    - fix plugin_updater for subsite

--- includes/core/class-plugin-updater.php
+++ includes/core/class-plugin-updater.php
@@ -158,6 +158,9 @@ if ( ! class_exists( 'um\core\Plugin_Updater' ) ) {
 					switch_to_blog( $site->blog_id );
 
 					$the_plugs = get_option( 'active_plugins' );
+					if( ! $the_plugs ) {
+						$the_plugs = array();
+					}
 					$the_plugs = array_merge( $the_plugs, $sitewide_plugins );
 
 					foreach ( $the_plugs as $key => $value ) {
