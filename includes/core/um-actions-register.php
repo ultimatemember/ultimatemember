8234fee3cf39f06a55cbbdc9a205423363fef48a
nikitasinelnikov <nsinelnikov.work@gmail.com>
2020-12-22 17:04:18 +0200

    - added a fix for getting status (fetch user again if not exists);

--- includes/core/um-actions-register.php
+++ includes/core/um-actions-register.php
@@ -64,8 +64,14 @@ function um_after_insert_user( $user_id, $args ) {
 		UM()->user()->set_registration_details( $args['submitted'], $args );
 	}
 
+	$status = um_user( 'status' );
+	if ( empty( $status ) ) {
+		um_fetch_user( $user_id );
+		$status = um_user( 'status' );
+	}
+
 	/* save user status */
-	UM()->user()->set_status( um_user( 'status' ) );
+	UM()->user()->set_status( $status );
 
 	/* create user uploads directory */
 	UM()->uploader()->get_upload_user_base_dir( $user_id, true );
