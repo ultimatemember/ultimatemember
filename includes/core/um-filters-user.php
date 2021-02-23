781a0fd16f7249bcfb25e1b22625587961d9803a
yuriinalivaiko <yuriinalivaiko.work@gmail.com>
2021-02-23 19:01:30 +0200

    Allow users who has capability 'edit_users' to approve a user or change a user status

--- includes/core/um-filters-user.php
+++ includes/core/um-filters-user.php
@@ -13,7 +13,7 @@ function um_admin_user_actions_hook( $actions, $user_id ) {
 	um_fetch_user( $user_id );
 
 	//if ( UM()->roles()->um_current_user_can( 'edit', $user_id ) ) {
-	if ( current_user_can( 'manage_options' ) ) {
+	if ( current_user_can( 'edit_users' ) ) {
 
 		if ( um_user( 'account_status' ) == 'awaiting_admin_review' ) {
 			$actions['um_approve_membership'] = array( 'label' => __( 'Approve Membership', 'ultimate-member' ) );
