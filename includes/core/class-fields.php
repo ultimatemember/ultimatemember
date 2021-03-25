19abbce9c364e04359bad478e0fc19c7ed1f62e7
Champ Camba <heychampsupertramp@gmail.com>
2021-03-25 17:13:49 +0800

    Fix confirm password placeholder when label is empty

--- includes/core/class-fields.php
+++ includes/core/class-fields.php
@@ -2395,9 +2395,13 @@ if ( ! class_exists( 'um\core\Fields' ) ) {
 								$name = $key;
 							}
 
-							if ( isset( $data['label'] ) ) {
+							if ( isset( $data['label'] ) || ! empty( $placeholder ) ) {
 
-								$placeholder = sprintf( __( 'Confirm %s', 'ultimate-member' ), $data['label'] );
+								if( ! empty( $placeholder ) && ! isset( $data['label'] ) ){
+									$placeholder = sprintf( __( 'Confirm %s', 'ultimate-member' ), $placeholder );
+								}else{
+									$placeholder = sprintf( __( 'Confirm %s', 'ultimate-member' ), $data['label'] );
+								}
 							}
 
 							$output .= '<input class="' . $this->get_class( $key, $data ) . '" type="' . esc_attr( $input ) . '" name="' . esc_attr( $name ) . '" id="' . esc_attr( $key . UM()->form()->form_suffix ) . '" value="' . $this->field_value( $key, $default, $data ) . '" placeholder="' . esc_attr( $placeholder ) . '" data-validate="' . esc_attr( $validate ) . '" data-key="' . esc_attr( $key ) . '" />';
