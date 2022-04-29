8fe836f8fad1cef66c58e0fdf7240c3f4729e7f9
Matt Gast <4122592+mattgast@users.noreply.github.com>
2022-04-29 14:08:22 +0200

    fix: Empty menu returning error

--- includes/core/um-filters-navmenu.php
+++ includes/core/um-filters-navmenu.php
@@ -11,6 +11,10 @@ if ( ! is_admin() ) {
 	 * @return array
 	 */
 	function um_add_custom_message_to_menu( $sorted_menu_items, $args ) {
+		//if empty
+		if ( empty( $sorted_menu_items ) ) {
+			return $sorted_menu_items;
+		}
 
 		if ( is_user_logged_in() ) {
 			um_fetch_user( get_current_user_id() );
