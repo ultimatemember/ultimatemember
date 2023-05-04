815561c760724bf56d902633605b63aa923f3d26
Mykyta Synelnikov <nsinelnikov.work@gmail.com>
2023-05-04 23:16:23 +0300

    - removed legacy;

--- includes/legacy/admin/core/packages/2.1.5/hooks.php
+++
@@ -1,5 +0,0 @@
-<?php if ( ! defined( 'ABSPATH' ) ) exit;
-
-return array(
-	'balance_field215' => 'balance_field215',
-);
--- templates/legacy/login-to-view.php
+++
@@ -1,7 +0,0 @@
-<?php if ( ! defined( 'ABSPATH' ) ) exit; ?>
-
-<div class="um-locked-content">
-
-	<div class="um-locked-content-msg"><?php echo htmlspecialchars_decode( $lock_text ); ?></div>
-
-</div>
