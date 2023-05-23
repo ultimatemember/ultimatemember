0600627a677ff2e57e12045c3a75c33c76bf9262
Mykyta Synelnikov <nsinelnikov.work@gmail.com>
2023-05-23 17:20:15 +0300

    - 2.6.2 version initial commit;

--- ultimate-member.php
+++ ultimate-member.php
@@ -3,7 +3,7 @@
 Plugin Name: Ultimate Member
 Plugin URI: http://ultimatemember.com/
 Description: The easiest way to create powerful online communities and beautiful user profiles with WordPress
-Version: 2.6.1
+Version: 2.6.2-alpha
 Author: Ultimate Member
 Author URI: http://ultimatemember.com/
 Text Domain: ultimate-member
@@ -11,14 +11,22 @@ Text Domain: ultimate-member
 
 defined( 'ABSPATH' ) || exit;
 
-require_once( ABSPATH . 'wp-admin/includes/plugin.php' );
+require_once ABSPATH . 'wp-admin/includes/plugin.php';
 $plugin_data = get_plugin_data( __FILE__ );
 
+// phpcs:disable Generic.NamingConventions.UpperCaseConstantName
 define( 'um_url', plugin_dir_url( __FILE__ ) );
 define( 'um_path', plugin_dir_path( __FILE__ ) );
 define( 'um_plugin', plugin_basename( __FILE__ ) );
 define( 'ultimatemember_version', $plugin_data['Version'] );
 define( 'ultimatemember_plugin_name', $plugin_data['Name'] );
+// phpcs:enable Generic.NamingConventions.UpperCaseConstantName
+
+define( 'UM_URL', plugin_dir_url( __FILE__ ) );
+define( 'UM_PATH', plugin_dir_path( __FILE__ ) );
+define( 'UM_PLUGIN', plugin_basename( __FILE__ ) );
+define( 'UM_VERSION', $plugin_data['Version'] );
+define( 'UM_PLUGIN_NAME', $plugin_data['Name'] );
 
 require_once 'includes/class-functions.php';
 require_once 'includes/class-init.php';
