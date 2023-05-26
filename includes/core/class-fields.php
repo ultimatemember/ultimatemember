6b4d85b3b0062f9b12ff93ddc53ba21366045ccb
yuriinalivaiko <yuriinalivaiko.work@gmail.com>
2023-05-26 09:59:39 +0300

    - fixed allowed_types in the image and file uploaders

--- includes/core/class-fields.php
+++ includes/core/class-fields.php
@@ -2978,6 +2978,9 @@ if ( ! class_exists( 'um\core\Fields' ) ) {
 							$set_id   = 0;
 							$set_mode = '';
 						}
+						if ( is_array( $allowed_types ) ) {
+							$allowed_types = implode( ',', $allowed_types );
+						}
 						$nonce  = wp_create_nonce( 'um_upload_nonce-' . $this->timestamp );
 						$output .= '<div class="um-single-image-preview ' . $crop_class . '"  data-crop="' . $crop_data . '" data-ratio="' . $ratio . '" data-min_width="' . $min_width . '" data-min_height="' . $min_height . '" data-coord=""><a href="javascript:void(0);" class="cancel"><i class="um-icon-close"></i></a><img src="" alt="" /><div class="um-clear"></div></div><div class="um-clear"></div>';
 						$output .= '<div class="um-single-image-upload" data-user_id="' . esc_attr( $_um_profile_id ) . '" data-nonce="' . $nonce . '" data-timestamp="' . esc_attr( $this->timestamp ) . '" data-icon="' . esc_attr( $icon ) . '" data-set_id="' . esc_attr( $set_id ) . '" data-set_mode="' . esc_attr( $set_mode ) . '" data-type="' . esc_attr( $type ) . '" data-key="' . esc_attr( $key ) . '" data-max_size="' . esc_attr( $max_size ) . '" data-max_size_error="' . esc_attr( $max_size_error ) . '" data-min_size_error="' . esc_attr( $min_size_error ) . '" data-extension_error="' . esc_attr( $extension_error ) . '"  data-allowed_types="' . esc_attr( $allowed_types ) . '" data-upload_text="' . esc_attr( $upload_text ) . '" data-max_files_error="' . esc_attr( $max_files_error ) . '" data-upload_help_text="' . esc_attr( $upload_help_text ) . '">' . $button_text . '</div>';
@@ -3076,6 +3079,9 @@ if ( ! class_exists( 'um\core\Fields' ) ) {
 						} else {
 							$set_id   = 0;
 							$set_mode = '';
+						}
+						if ( is_array( $allowed_types ) ) {
+							$allowed_types = implode( ',', $allowed_types );
 						}
 						$output .= '<div class="um-single-file-preview">
 										<a href="javascript:void(0);" class="cancel"><i class="um-icon-close"></i></a>
