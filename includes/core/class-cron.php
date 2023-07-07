cb3897c48bb3d0f43f3f1bef5b3ac41a944c7f47
Mykyta Synelnikov <nsinelnikov.work@gmail.com>
2023-07-07 13:34:55 +0300

    - UM cron class review; - schedule events on the first install; - make an event's starting time base on recurrence;

--- includes/core/class-cron.php
+++ includes/core/class-cron.php
@@ -1,11 +1,11 @@
 <?php
 namespace um\core;
 
-// Exit if accessed directly
-if ( ! defined( 'ABSPATH' ) ) exit;
+if ( ! defined( 'ABSPATH' ) ) {
+	exit;
+}
 
 if ( ! class_exists( 'um\core\Cron' ) ) {
-
 
 	/**
 	 * Class Cron
@@ -13,110 +13,114 @@ if ( ! class_exists( 'um\core\Cron' ) ) {
 	 */
 	class Cron {
 
-
 		/**
 		 * Cron constructor.
 		 */
 		public function __construct() {
-
-			/**
-			 * UM hook
-			 *
-			 * @type filter
-			 * @title um_cron_disable
-			 * @description Make UM Cron Actions Enabled or Disabled
-			 * @input_vars
-			 * [{"var":"$cron_disable","type":"bool","desc":"Disable UM Cron?"}]
-			 * @change_log
-			 * ["Since: 2.0"]
-			 * @usage add_filter( 'um_cron_disable', 'function_name', 10, 1 );
-			 * @example
-			 * <?php
-			 * add_filter( 'um_cron_disable', 'my_cron_disable', 10, 1 );
-			 * function my_predefined_field( $cron_disable ) {
-			 *     // your code here
-			 *     return $cron_disable;
-			 * }
-			 * ?>
-			 */
-			$um_cron = apply_filters( 'um_cron_disable', false );
-			if ( $um_cron ) {
-				return;
-			}
-            
 			add_filter( 'cron_schedules', array( $this, 'add_schedules' ) );
-			add_action( 'wp', array( $this, 'schedule_Events' ) );
+			add_action( 'wp', array( $this, 'schedule_events' ) );
 		}
 
+		/**
+		 * @return bool
+		 */
+		private function cron_disabled() {
+			/**
+			 * Filters variable for disable Ultimate Member WP Cron actions.
+			 *
+			 * @since 2.0
+			 * @hook  um_cron_disable
+			 *
+			 * @param {bool} $is_disabled Shortcode arguments.
+			 *
+			 * @return {bool} Do Cron actions are disabled? True for disable.
+			 *
+			 * @example <caption>Disable all Ultimate Member WP Cron actions.</caption>
+			 * add_filter( 'um_cron_disable', '__return_true' );
+			 */
+			return apply_filters( 'um_cron_disable', false );
+		}
 
 		/**
+		 * Adds once weekly to the existing schedules.
+		 *
 		 * @param array $schedules
 		 *
 		 * @return array
 		 */
 		public function add_schedules( $schedules = array() ) {
+			if ( $this->cron_disabled() ) {
+				return $schedules;
+			}
 
-			// Adds once weekly to the existing schedules.
 			$schedules['weekly'] = array(
 				'interval' => 604800,
-				'display'  => __( 'Once Weekly', 'ultimate-member' )
+				'display'  => __( 'Once Weekly', 'ultimate-member' ),
 			);
 
 			return $schedules;
 		}
 
-
 		/**
 		 *
 		 */
-		public function schedule_Events() {
+		public function schedule_events() {
+			if ( $this->cron_disabled() ) {
+				return;
+			}
+
 			$this->weekly_events();
 			$this->daily_events();
 			$this->twicedaily_events();
 			$this->hourly_events();
 		}
 
-
 		/**
 		 *
 		 */
 		private function weekly_events() {
+			$sunday_start   = wp_date( 'w' );
+			$week_start     = $sunday_start - absint( get_option( 'start_of_week' ) );
+			$week_start_day = strtotime( '-' . $week_start . ' days' );
+			$time           = mktime( 0, 0, 0, wp_date( 'm', $week_start_day ), wp_date( 'd', $week_start_day ), wp_date( 'Y', $week_start_day ) );
 			if ( ! wp_next_scheduled( 'um_weekly_scheduled_events' ) ) {
-				wp_schedule_event( current_time( 'timestamp' ), 'weekly', 'um_weekly_scheduled_events' );
+				wp_schedule_event( $time, 'weekly', 'um_weekly_scheduled_events' );
 			}
 		}
-
 
 		/**
 		 *
 		 */
 		private function daily_events() {
 			if ( ! wp_next_scheduled( 'um_daily_scheduled_events' ) ) {
-				wp_schedule_event( current_time( 'timestamp' ), 'daily', 'um_daily_scheduled_events' );
+				$time = mktime( 0, 0, 0, wp_date( 'm' ), wp_date( 'd' ), wp_date( 'Y' ) );
+				wp_schedule_event( $time, 'daily', 'um_daily_scheduled_events' );
 			}
 		}
-
 
 		/**
 		 *
 		 */
 		private function twicedaily_events() {
 			if ( ! wp_next_scheduled( 'um_twicedaily_scheduled_events' ) ) {
-				wp_schedule_event( current_time( 'timestamp' ), 'twicedaily', 'um_twicedaily_scheduled_events' );
+				$time = mktime( 0, 0, 0, wp_date( 'm' ), wp_date( 'd' ), wp_date( 'Y' ) );
+				wp_schedule_event( $time, 'twicedaily', 'um_twicedaily_scheduled_events' );
 			}
 		}
-
 
 		/**
 		 *
 		 */
 		private function hourly_events() {
 			if ( ! wp_next_scheduled( 'um_hourly_scheduled_events' ) ) {
-				wp_schedule_event( current_time( 'timestamp' ), 'hourly', 'um_hourly_scheduled_events' );
+				$time = mktime( wp_date( 'H' ), 0, 0, wp_date( 'm' ), wp_date( 'd' ), wp_date( 'Y' ) );
+				wp_schedule_event( $time, 'hourly', 'um_hourly_scheduled_events' );
 			}
 		}
 
-
+		/**
+		 * Breaks all Ultimate Member registered schedule events.
+		 */
 		public function unschedule_events() {
 			wp_clear_scheduled_hook( 'um_weekly_scheduled_events' );
 			wp_clear_scheduled_hook( 'um_daily_scheduled_events' );
