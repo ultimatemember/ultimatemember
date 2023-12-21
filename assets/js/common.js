92c46ec93174ec0cfd260f966754b551f5daac5d
yuriinalivaiko <yuriinalivaiko.work@gmail.com>
2023-12-21 20:14:22 +0200

    - fixed AJAX conflict

--- assets/js/common.js
+++ assets/js/common.js
@@ -116,27 +116,6 @@ UM.common = {
 	}
 }
 
-jQuery.ajaxSetup({
-	beforeSend: function( jqXHR, settings ) {
-		if ( settings.processData ) {
-			if ( settings.data !== '' ) {
-				settings.data += '&um_current_locale=' + um_common_variables.locale;
-			} else {
-				settings.data = 'um_current_locale=' + um_common_variables.locale;
-			}
-		} else {
-			settings.data = jQuery.extend(
-				settings.data,
-				{
-					um_current_locale: um_common_variables.locale
-				}
-			);
-		}
-
-		return true;
-	}
-});
-
 jQuery(document).on( 'ajaxStart', function() {
 	UM.common.tipsy.hide();
 });
