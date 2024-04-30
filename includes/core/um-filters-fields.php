93de64ed75dcb59ffb6ce5d65091524f3a6d4095
ashubawork <andrew.shuba2@gmail.com>
2024-04-30 13:08:23 +0300

    - small fix

--- includes/core/um-filters-fields.php
+++ includes/core/um-filters-fields.php
@@ -850,7 +850,7 @@ function um_profile_field_filter_xss_validation( $value, $data, $type = '' ) {
 				$value = 0;
 			} else {
 				if ( 5 === absint( $data['number'] ) ) {
-					if ( ! in_array( $value, range( 1, 5 ), true ) ) {
+					if ( ! in_array( absint( $value ), range( 1, 5 ), true ) ) {
 						$value = 0;
 					}
 				} elseif ( 10 === $data['number'] ) {
