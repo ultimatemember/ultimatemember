17a85af9226ed47b36220596d8b24bcee63f339f
Mykyta Synelnikov <nsinelnikov.work@gmail.com>
2024-07-09 18:53:42 +0300

    - changed skeleton for list type and updated cover photo option displayed;

--- includes/admin/templates/directory/profile.php
+++ includes/admin/templates/directory/profile.php
@@ -21,13 +21,13 @@ $cover_photos_field = array(
 );
 if ( defined( 'UM_DEV_MODE' ) && UM_DEV_MODE && UM()->options()->get( 'enable_new_ui' ) ) {
 	$cover_photos_field = array(
-		'id'      => '_um_cover_photos',
-		'type'    => 'checkbox',
-		'label'   => __( 'Enable Cover Photo', 'ultimate-member' ),
-		'value'   => (bool) get_post_meta( $post_id, '_um_cover_photos', true ),
+		'id'             => '_um_cover_photos',
+		'type'           => 'checkbox',
+		'label'          => __( 'Enable Cover Photo', 'ultimate-member' ),
+		'value'          => (bool) get_post_meta( $post_id, '_um_cover_photos', true ),
 		'checkbox_label' => __( 'Enable Cover Photo (for grid view only).', 'ultimate-member' ),
 		'description'    => __( 'If turned on, the users cover photo will appear in the directory.', 'ultimate-member' ),
-		'conditional' => array( '_um_view_types_grid', '~', 1 ),
+		'conditional'    => array( '_um_view_types_grid', '~', 1 ),
 	);
 }
 
