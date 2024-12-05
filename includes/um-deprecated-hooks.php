f8871c577279c3565487fdc5d8428a27991d5417
Mykyta Synelnikov <nsinelnikov.work@gmail.com>
2024-12-05 17:41:55 +0200

    * wpcs;

--- includes/um-deprecated-hooks.php
+++ includes/um-deprecated-hooks.php
@@ -111,6 +111,18 @@ if ( ! defined( 'ABSPATH' ) ) {
  */
 
 /**
+ * Filters the plugin's textdomain.
+ *
+ * @param {string} $domain Plugin's textdomain.
+ *
+ * @return {string} Maybe changed plugin's textdomain.
+ *
+ * @since 1.3.x
+ * @depecated 2.9.2 Fully deprecated because minimum required WP version is 5.5, but we cannot use `load_plugin_textdomain()` function since 4.6.0 if the plugin is situated in wp.org plugins directory.
+ * @hook um_language_textdomain
+ */
+
+/**
  * Fires for displaying content in supporting header row on User Profile.
  *
  * @param {array} $args User Profile data.
@@ -138,16 +150,4 @@ if ( ! defined( 'ABSPATH' ) ) {
  * @since 1.3.x
  * @depecated 3.0.0 Please use 'um_allowed_default_file_types' hook instead.
  * @hook  um_allowed_file_types
- */
-
-/**
- * Filters the plugin's textdomain.
- *
- * @param {string} $domain Plugin's textdomain.
- *
- * @return {string} Maybe changed plugin's textdomain.
- *
- * @since 1.3.x
- * @depecated 2.9.2 Fully deprecated because minimum required WP version is 5.5, but we cannot use `load_plugin_textdomain()` function since 4.6.0 if the plugin is situated in wp.org plugins directory.
- * @hook um_language_textdomain
  */
