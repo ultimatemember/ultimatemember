2c01e25b5cafa065e02743e9f0f1548a7f9c9db4
Mykyta Synelnikov <nsinelnikov.work@gmail.com>
2025-02-12 18:44:46 +0200

    Enhance form preview options and improve code consistency
    Added separate buttons for 'Live Preview Screen' and 'Live Preview Mobile' in the form builder for better user experience. Updated coding standards for consistency, including proper handling of PHP tags and escaping functions. These changes improve readability, maintainability, and enhance functionality.
    
    Issue #1646

--- includes/admin/templates/form/builder.php
+++ includes/admin/templates/form/builder.php
@@ -1,20 +1,25 @@
-<?php if ( ! defined( 'ABSPATH' ) ) {
+<?php
+if ( ! defined( 'ABSPATH' ) ) {
 	exit;
 }
 
 if ( empty( UM()->builder()->form_id ) ) {
 	UM()->builder()->form_id = $this->form_id;
-} ?>
+}
+?>
 
 <div class="um-admin-builder" data-form_id="<?php echo esc_attr( UM()->builder()->form_id ); ?>">
 
 	<div class="um-admin-drag-ctrls-demo um-admin-drag-ctrls">
-
-		<a href="javascript:void(0);" class="active" data-modal="UM_preview_form" data-modal-size="smaller"
-		   data-dynamic-content="um_admin_preview_form" data-arg1="<?php echo esc_attr( get_the_ID() ); ?>" data-arg2="">
-			<?php esc_html_e( 'Live Preview', 'ultimate-member' ); ?>
+		<a href="#" class="active" data-modal="UM_preview_form" data-modal-size="larger"
+			data-dynamic-content="um_admin_preview_form" data-arg1="<?php echo esc_attr( get_the_ID() ); ?>" data-arg2="">
+			<?php esc_html_e( 'Live Preview Screen', 'ultimate-member' ); ?>
 		</a>
 
+		<a href="#" class="active" data-modal="UM_preview_form" data-modal-size="smaller"
+			data-dynamic-content="um_admin_preview_form" data-arg1="<?php echo esc_attr( get_the_ID() ); ?>" data-arg2="">
+			<?php esc_html_e( 'Live Preview Mobile', 'ultimate-member' ); ?>
+		</a>
 	</div>
 
 	<div class="clear"></div>
@@ -26,7 +31,7 @@ if ( empty( UM()->builder()->form_id ) ) {
 		</div>
 
 		<div class="um-admin-drag-addrow um-tip-n" title="<?php esc_attr_e( 'Add Master Row', 'ultimate-member' ); ?>"
-		     data-row_action="add_row">
+			data-row_action="add_row">
 			<i class="um-icon-plus"></i>
 		</div>
 
