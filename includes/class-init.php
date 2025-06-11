6bbb421778a8e210f08b7d7dee7c4fd053515560
Mykyta Synelnikov <nsinelnikov.work@gmail.com>
2025-06-11 14:52:21 +0300

    Update bbPress method and plugin version number
    Renamed the bbPress method in class-init.php for consistency and clarity. Bumped the plugin version to 3.0.0-alpha-20250611 in ultimate-member.php to reflect the latest changes.

--- includes/class-init.php
+++ includes/class-init.php
@@ -12,7 +12,7 @@ if ( ! class_exists( 'UM' ) ) {
 	 * @class UM
 	 * @version 2.0
 	 *
-	 * @method UM_bbPress_API bbPress_API()
+	 * @method UM_bbPress bbPress()
 	 * @method UM_Followers_API Followers_API()
 	 * @method UM_ForumWP ForumWP() Reviewed
 	 * @method UM_Friends_API Friends_API()
