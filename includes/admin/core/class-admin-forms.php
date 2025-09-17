e26bd8eafb1ea2fc08097e8565a5e744b045c930
Mykyta Synelnikov <nsinelnikov.work@gmail.com>
2025-09-17 15:37:13 +0300

    Fix default page creation check in admin form logic
    Updated logic to handle the 'predefined' field flag when determining default page creation. This ensures buttons for creating default pages only appear under the correct conditions, improving reliability and consistency.

--- includes/admin/core/class-admin-forms.php
+++ includes/admin/core/class-admin-forms.php
@@ -1164,9 +1164,11 @@ if ( ! class_exists( 'um\admin\core\Admin_Forms' ) ) {
 			}
 
 			$button = '';
-			$slug   = str_replace( 'core_', '', $field_data['id'] );
-			if ( ! um_get_predefined_page_id( $slug ) || 'publish' !== get_post_status( um_get_predefined_page_id( $slug ) ) ) {
-				$button = '&nbsp;<a href="' . esc_url( add_query_arg( array( 'um_adm_action' => 'install_predefined_page', 'um_page_slug' => $slug, '_wpnonce' => wp_create_nonce( 'install_predefined_page' ), ) ) ) . '" class="button button-primary">' . esc_html__( 'Create Default', 'ultimate-member' ) . '</a>';
+			if ( ! array_key_exists( 'predefined', $field_data ) || false !== $field_data['predefined'] ) {
+				$slug = str_replace( 'core_', '', $field_data['id'] );
+				if ( ! um_get_predefined_page_id( $slug ) || 'publish' !== get_post_status( um_get_predefined_page_id( $slug ) ) ) {
+					$button = '&nbsp;<a href="' . esc_url( add_query_arg( array( 'um_adm_action' => 'install_predefined_page', 'um_page_slug' => $slug, '_wpnonce' => wp_create_nonce( 'install_predefined_page' ), ) ) ) . '" class="button button-primary">' . esc_html__( 'Create Default', 'ultimate-member' ) . '</a>';
+				}
 			}
 
 			$html = "$hidden<select $multiple $id_attr $name_attr $class_attr $data_attr>$options</select>$button";
