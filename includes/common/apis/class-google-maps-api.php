72dd4f3c519706e869cbf9bbd06efb3ec9018164
Mykyta Synelnikov <nsinelnikov.work@gmail.com>
2026-03-20 21:07:56 +0200

    Fix Google Maps locale handling and add language parameter
    Adjusted locale extraction to correctly use the first locale segment instead of the second. Added a language parameter to the Google Maps API script initialization to support localized map display.

--- includes/common/apis/class-google-maps-api.php
+++ includes/common/apis/class-google-maps-api.php
@@ -26,8 +26,8 @@ class Google_Maps_Api {
 				$locale = str_replace( '_', '-', $locale );
 				if ( ! in_array( $locale, $locales, true ) ) {
 					$locale = explode( '-', $locale );
-					if ( isset( $locale[1] ) ) {
-						$locale = $locale[1];
+					if ( isset( $locale[0] ) ) {
+						$locale = $locale[0];
 					}
 				}
 			}
@@ -55,6 +55,7 @@ class Google_Maps_Api {
 			(g=>{var h,a,k,p="The Google Maps JavaScript API",c="google",l="importLibrary",q="__ib__",m=document,b=window;b=b[c]||(b[c]={});var d=b.maps||(b.maps={}),r=new Set,e=new URLSearchParams,u=()=>h||(h=new Promise(async(f,n)=>{await (a=m.createElement("script"));e.set("libraries",[...r]+"");for(k in g)e.set(k.replace(/[A-Z]/g,t=>"_"+t[0].toLowerCase()),g[k]);e.set("callback",c+".maps."+q);a.src=`https://maps.${c}apis.com/maps/api/js?`+e;d[q]=f;a.onerror=()=>h=n(Error(p+" could not load."));a.nonce=m.querySelector("script[nonce]")?.nonce||"";m.head.append(a)}));d[l]?console.warn(p+" only loads once. Ignoring:",g):d[l]=(f,...n)=>r.add(f)&&u().then(()=>d[l](f,...n))})({
 				key: "<?php echo esc_js( UM()->options()->get( 'um_google_maps_js_api_key' ) ); ?>",
 				v: "weekly",
+				language: "<?php echo esc_js( self::get_locale() ); ?>"
 			});
 			<?php
 			$script = ob_get_clean();
