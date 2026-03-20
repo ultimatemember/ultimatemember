1c0b389baf040f184f6f439fd0c135ec0e037b88
Mykyta Synelnikov <nsinelnikov.work@gmail.com>
2026-03-20 17:02:29 +0200

    Refactor role-based filter logic in directory class
    Simplified and reorganized the conditional checks for role-based filters, improving readability and maintainability. Consolidated redundant logic to remove unnecessary nesting and streamline flow. This change does not alter functionality but enhances code clarity.

--- includes/common/class-directory.php
+++ includes/common/class-directory.php
@@ -1232,57 +1232,54 @@ class Directory extends Directory_Config {
 
 				$attrs['options'] = array_intersect_key( array_map( 'trim', $attrs['options'] ), array_flip( $values_array ) );
 			}
-		} else {
-			if ( 'role' === $filter ) {
-				$values_array    = array();
-				$shortcode_roles = get_post_meta( $directory_id, '_um_roles', true );
-				$shortcode_roles = maybe_unserialize( $shortcode_roles );
-				if ( $admin ) {
-					$editable_roles = array_reverse( get_editable_roles() );
-					if ( ! empty( $shortcode_roles ) && is_array( $shortcode_roles ) ) {
-						foreach ( $editable_roles as $role => $details ) {
-							if ( in_array( $role, $shortcode_roles, true ) ) {
-								$values_array[ $role ] = translate_user_role( $details['name'] );
-							}
-						}
-					} else {
-						foreach ( $editable_roles as $role => $details ) {
+		} elseif ( 'role' === $filter ) {
+			$values_array    = array();
+			$shortcode_roles = get_post_meta( $directory_id, '_um_roles', true );
+			$shortcode_roles = maybe_unserialize( $shortcode_roles );
+			if ( $admin ) {
+				$editable_roles = array_reverse( get_editable_roles() );
+				if ( ! empty( $shortcode_roles ) && is_array( $shortcode_roles ) ) {
+					foreach ( $editable_roles as $role => $details ) {
+						if ( in_array( $role, $shortcode_roles, true ) ) {
 							$values_array[ $role ] = translate_user_role( $details['name'] );
 						}
 					}
 				} else {
-					$um_roles = UM()->roles()->get_roles();
-					if ( true !== $disable_filters_pre_query ) {
-						$users_roles = count_users();
-						$roles_exist = ( ! empty( $users_roles['avail_roles'] ) && is_array( $users_roles['avail_roles'] ) ) ? array_keys( array_filter( $users_roles['avail_roles'] ) ) : array();
-
-						foreach ( $um_roles as $key => $value ) {
-							if ( ! empty( $shortcode_roles ) && is_array( $shortcode_roles ) ) {
-								if ( in_array( $key, $roles_exist, true ) && in_array( $key, $shortcode_roles, true ) ) {
-									$values_array[ $key ] = $value;
-								}
-							} elseif ( in_array( $key, $roles_exist, true ) ) {
-								$values_array[ $key ] = $value;
-							}
-						}
-					} elseif ( ! empty( $shortcode_roles ) && is_array( $shortcode_roles ) ) {
-						foreach ( $um_roles as $key => $value ) {
-							if ( in_array( $key, $shortcode_roles, true ) ) {
-								$values_array[ $key ] = $value;
-							}
-						}
-					} else {
-						$values_array = $um_roles;
+					foreach ( $editable_roles as $role => $details ) {
+						$values_array[ $role ] = translate_user_role( $details['name'] );
 					}
 				}
+			} else {
+				$um_roles = UM()->roles()->get_roles();
+				if ( true !== $disable_filters_pre_query ) {
+					$users_roles = count_users();
+					$roles_exist = ( ! empty( $users_roles['avail_roles'] ) && is_array( $users_roles['avail_roles'] ) ) ? array_keys( array_filter( $users_roles['avail_roles'] ) ) : array();
 
-				$attrs['options'] = $values_array;
+					foreach ( $um_roles as $key => $value ) {
+						if ( ! empty( $shortcode_roles ) && is_array( $shortcode_roles ) ) {
+							if ( in_array( $key, $roles_exist, true ) && in_array( $key, $shortcode_roles, true ) ) {
+								$values_array[ $key ] = $value;
+							}
+						} elseif ( in_array( $key, $roles_exist, true ) ) {
+							$values_array[ $key ] = $value;
+						}
+					}
+				} elseif ( ! empty( $shortcode_roles ) && is_array( $shortcode_roles ) ) {
+					foreach ( $um_roles as $key => $value ) {
+						if ( in_array( $key, $shortcode_roles, true ) ) {
+							$values_array[ $key ] = $value;
+						}
+					}
+				} else {
+					$values_array = $um_roles;
+				}
 			}
 
-			if ( ! $admin && true !== $disable_filters_pre_query && empty( $attrs['disable_filters_pre_query'] ) ) {
+			$attrs['options'] = $values_array;
+		} elseif ( ! $admin && true !== $disable_filters_pre_query && empty( $attrs['disable_filters_pre_query'] ) ) {
+			if ( in_array( $attrs['type'], array( 'select', 'multiselect', 'checkbox', 'radio' ), true ) ) {
 				$values_array = $this->pre_filter_query( $attrs['metakey'], $directory_data );
-
-				if ( ! empty( $values_array ) && in_array( $attrs['type'], array( 'select', 'multiselect', 'checkbox', 'radio' ), true ) ) {
+				if ( ! empty( $values_array ) ) {
 					$values_array = array_map(
 						function ( $item ) {
 							$item = maybe_unserialize( $item );
@@ -1292,12 +1289,12 @@ class Directory extends Directory_Config {
 					);
 					$values_array = array_unique( array_merge( ...$values_array ) );
 				}
+			}
 
-				if ( empty( $option_pairs ) ) {
-					$attrs['options'] = array_intersect( array_map( 'stripslashes', array_map( 'trim', $attrs['options'] ) ), $values_array );
-				} else {
-					$attrs['options'] = array_intersect_key( array_map( 'trim', $attrs['options'] ), array_flip( $values_array ) );
-				}
+			if ( empty( $option_pairs ) ) {
+				$attrs['options'] = array_intersect( array_map( 'stripslashes', array_map( 'trim', $attrs['options'] ) ), $values_array );
+			} else {
+				$attrs['options'] = array_intersect_key( array_map( 'trim', $attrs['options'] ), array_flip( $values_array ) );
 			}
 		}
 
